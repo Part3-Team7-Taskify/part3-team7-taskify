453b5e4e3f64bb9e77cfda80d6fd9bc8ef5f89fd
Squarecat-meow <Yozumina@serafuku.moe>
2025-07-24 16:25:23 +0900

    fix(Input): Input 컴포넌트 리팩토링

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,13 +1,21 @@
-import { FC } from 'react';
+import { ComponentPropsWithoutRef, FC } from 'react';
 
-interface InputType {
+interface InputType extends Omit<ComponentPropsWithoutRef<'input'>, 'type'> {
   type: 'text' | 'password' | 'title' | 'email';
   placeholder: string;
   icon?: React.ReactNode;
   iconOnClick?: () => void;
+  className?: string;
 }
 
-export const Input: FC<InputType> = ({ type, placeholder, icon, iconOnClick }) => {
+export const Input: FC<InputType> = ({
+  type,
+  placeholder,
+  icon,
+  iconOnClick,
+  className,
+  ...rest
+}) => {
   const inputType = {
     text: 'text',
     password: 'password',
@@ -15,13 +23,21 @@ export const Input: FC<InputType> = ({ type, placeholder, icon, iconOnClick }) =
     email: 'email',
   };
   return (
-    <div className='relative'>
+    <div className='relative w-full'>
       <input
         type={inputType[type]}
         placeholder={placeholder}
-        className={`${type === 'title' && 'font-bold'} w-full p-3.5 rounded-lg border border-gray-200 bg-white text-black invalid:border-red-500`}
+        className={`${type === 'title' && 'font-bold'} w-full p-3.5 rounded-lg border border-gray-200 bg-white text-black invalid:border-red-500 ${className}`}
+        {...rest}
       />
-      <button onClick={iconOnClick} className='absolute top-1/2 -translate-y-1/2 right-4'>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          if (iconOnClick) iconOnClick();
+        }}
+        tabIndex={1}
+        className='absolute top-1/2 -translate-y-1/2 right-4 cursor-pointer'
+      >
         {icon}
       </button>
     </div>
