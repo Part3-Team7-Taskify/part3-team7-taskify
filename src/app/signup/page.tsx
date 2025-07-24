16390609632065cb2509db624f616a8aec644004
Squarecat-meow <Yozumina@serafuku.moe>
2025-07-24 17:44:33 +0900

    feat(Signup): 회원가입 페이지 완성

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -36,6 +36,8 @@ const Page = () => {
 
   const onSubmit: SubmitHandler<SignupFormType> = async (e) => {
     try {
+      if (e.password !== e.passwordConfirm)
+        setError('passwordConfirm', { type: 'manual', message: '비밀번호가 일치하지 않습니다.' });
       const response = await apiClient.post('users', e);
       if (response.status === 201) {
         setAccessToken(response.data.accessToken);
@@ -47,7 +49,7 @@ const Page = () => {
         if (err.status === 404) {
           setError('root', { type: 'manual', message: '존재하지 않는 유저입니다.' });
         } else if (err.status === 409) {
-          setError('root', { type: 'manual', message: '이미 사용중인 이메일입니다.' });
+          setError('email', { type: 'manual', message: '이미 사용중인 이메일입니다.' });
         } else {
           setError('root', { type: 'manual', message: '로그인 오류입니다.' });
         }
@@ -56,7 +58,7 @@ const Page = () => {
   };
   return (
     <>
-      <main>
+      <main className='w-screen h-screen grid place-items-center'>
         <section className='w-[351px] md:w-[520px] grid place-items-center gap-4'>
           <Image src={Logo} alt='Taskify 로고' priority={true} />
           <span>오늘도 만나서 반가워요!</span>
@@ -66,10 +68,16 @@ const Page = () => {
               <Input
                 type='email'
                 placeholder='이메일을 입력해 주세요'
-                {...register('email', { required: true })}
+                {...register('email', {
+                  required: true,
+                  pattern: {
+                    value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+                    message: '올바른 이메일 형식을 입력해주세요.',
+                  },
+                })}
               />
-              {errors.root && (
-                <span className='absolute right-0 top-20 text-red-500'>{errors.root.message}</span>
+              {errors.email && (
+                <span className='absolute right-0 top-20 text-red-500'>{errors.email.message}</span>
               )}
             </label>
             <label className='w-full relative'>
@@ -77,10 +85,16 @@ const Page = () => {
               <Input
                 type='text'
                 placeholder='닉네임을 입력해 주세요'
-                {...register('nickname', { required: true })}
+                className={`${errors.nickname && 'border-red-500'}`}
+                {...register('nickname', {
+                  required: true,
+                  minLength: { value: 10, message: '닉네임은 10자 이하여야 합니다.' },
+                })}
               />
-              {errors.root && (
-                <span className='absolute right-0 top-20 text-red-500'>{errors.root.message}</span>
+              {errors.nickname && (
+                <span className='absolute right-0 top-20 text-red-500'>
+                  {errors.nickname.message}
+                </span>
               )}
             </label>
             <label className='w-full relative'>
@@ -90,8 +104,17 @@ const Page = () => {
                 placeholder='8자 이상 입력해 주세요'
                 icon={showPassword ? <ShowPasswordIcon /> : <HidePasswordIcon />}
                 iconOnClick={() => setShowPassword(!showPassword)}
-                {...register('password', { required: true })}
+                className={`${errors.password && 'border-red-500'}`}
+                {...register('password', {
+                  required: true,
+                  minLength: { value: 8, message: '비밀번호는 8자 이상이어야 합니다.' },
+                })}
               />
+              {errors.password && (
+                <span className='absolute right-0 top-20 text-red-500'>
+                  {errors.password.message}
+                </span>
+              )}
             </label>
             <label className='w-full relative'>
               <span>비밀번호 확인</span>
@@ -100,8 +123,20 @@ const Page = () => {
                 placeholder='비밀번호를 한번 더 입력해 주세요'
                 icon={showPasswordConfirm ? <ShowPasswordIcon /> : <HidePasswordIcon />}
                 iconOnClick={() => setShowPasswordConfirm(!showPasswordConfirm)}
-                {...register('passwordConfirm', { required: true })}
+                className={`${errors.passwordConfirm && 'border-red-500'}`}
+                {...register('passwordConfirm', {
+                  required: true,
+                  deps: 'password',
+                  validate: (value, { password }) => {
+                    if (value !== password) return '비밀번호가 일치하지 않습니다.';
+                  },
+                })}
               />
+              {errors.passwordConfirm && (
+                <span className='absolute right-0 top-20 text-red-500'>
+                  {errors.passwordConfirm.message}
+                </span>
+              )}
             </label>
             <Button type={!isDirty || !isValid ? 'disabled' : 'primary'} className='w-full mt-4'>
               로그인
