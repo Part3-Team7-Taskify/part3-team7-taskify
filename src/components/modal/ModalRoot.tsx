271387c7e101617c77351f83eec0a42791413446
Squarecat-meow <Yozumina@serafuku.moe>
2025-08-01 08:53:51 +0900

    feat(modal): Modal 애니메이션 추가

--- src/components/modal/ModalRoot.tsx
+++ src/components/modal/ModalRoot.tsx
@@ -16,12 +16,19 @@ type ModalPropsType = {
   buttonCallbackVer2?: () => void;
 };
 
-export const ModalPropsContext = createContext<ModalPropsType>({
+interface IModalVisiblity {
+  isVisible: boolean;
+  setIsVisible: (state: boolean) => void;
+}
+
+export const ModalPropsContext = createContext<ModalPropsType & IModalVisiblity>({
   title: undefined,
   meatballMenu: undefined,
   children: null,
   modalButtonType: 'one',
   modalOpenState: false,
+  isVisible: false,
+  setIsVisible: () => {},
   modalOpenSetState: () => {},
   buttonCallback: () => {},
   buttonCallbackVer2: () => {},
@@ -38,10 +45,23 @@ export const ModalRoot: FC<ModalPropsType> = ({
   buttonCallbackVer2, //칼럼 수정용 콜백
 }) => {
   const [portalElement, setPortalElement] = useState<Element | null>(null);
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
+    if (modalOpenState) {
+      setTimeout(() => {
+        setIsVisible(true);
+      }, 20);
+    }
     setPortalElement(document.getElementById('portal'));
   }, [modalOpenState]);
+
+  const closeModal = (state: boolean) => {
+    setIsVisible(state);
+    setTimeout(() => {
+      modalOpenSetState(state);
+    }, 300);
+  };
 
   return (
     <>
@@ -51,10 +71,12 @@ export const ModalRoot: FC<ModalPropsType> = ({
           meatballMenu,
           children,
           modalOpenState,
-          modalOpenSetState,
+          modalOpenSetState: (state) => closeModal(state),
           modalButtonType,
           buttonCallback,
           buttonCallbackVer2,
+          isVisible,
+          setIsVisible,
         }}
       >
         {modalOpenState && portalElement ? createPortal(<ModalWrapper />, portalElement) : null}
@@ -66,28 +88,35 @@ export const ModalRoot: FC<ModalPropsType> = ({
 const ModalWrapper = () => {
   return (
     <div className='fixed'>
-      <ModalBackdrop />
-      <ModalWindow />
+      <div className='z-[1]'>
+        <ModalBackdrop />
+      </div>
+      <div className='z-[2] pointer-events-none'>
+        <ModalWindow />
+      </div>
     </div>
   );
 };
 
 const ModalBackdrop = () => {
-  const { modalOpenSetState } = useContext(ModalPropsContext);
+  const { isVisible, modalOpenSetState } = useContext(ModalPropsContext);
   return (
     <div
-      className='absolute top-0 left-0 w-screen h-screen bg-black/30 backdrop-blur-xs z-[1]'
+      className={`absolute top-0 left-0 w-screen h-screen transition-all duration-200 ease-out ${isVisible ? 'backdrop-blur-xs bg-black/30' : 'backdrop-blur-none bg-transparent'}`}
       onClick={() => modalOpenSetState(false)}
     />
   );
 };
 
 const ModalWindow = () => {
-  const { title, meatballMenu, children, modalOpenSetState } = useContext(ModalPropsContext);
+  const { title, meatballMenu, children, isVisible, modalOpenSetState } =
+    useContext(ModalPropsContext);
 
   return (
-    <div className='w-screen h-screen grid place-content-center'>
-      <div className={`w-fit h-fit p-6 bg-white rounded-lg z-[2]`}>
+    <div className='w-screen h-screen grid place-content-center perspective-midrange'>
+      <div
+        className={`w-fit h-fit p-6 bg-white rounded-lg pointer-events-auto transition-all duration-200 ease-out ${isVisible ? 'mb-0 opacity-100 rotate-x-0' : 'mb-12 opacity-0 rotate-x-4'}`}
+      >
         <div className='flex items-center gap-6'>
           <h1 className='flex-1 text-xl font-bold'>{title}</h1>
           {meatballMenu && (
