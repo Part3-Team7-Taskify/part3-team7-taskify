fbeaade4addfe71f7a10303108a1e43c1fb225bd
Squarecat-meow <Yozumina@serafuku.moe>
2025-07-31 18:21:01 +0900

    feat(PaginationButton): disabled Props 추가

--- src/components/button/PaginationButton.tsx
+++ src/components/button/PaginationButton.tsx
@@ -1,20 +1,26 @@
 export const PaginationButton = ({
   onClickLeft,
   onClickRight,
+  disabledLeft,
+  disabledRight,
 }: {
-  onClickLeft?: () => void;
-  onClickRight?: () => void;
+  onClickLeft: () => void;
+  onClickRight: () => void;
+  disabledLeft: boolean;
+  disabledRight: boolean;
 }) => {
   return (
     <>
       <button
-        className='bg-white w-9 h-9 text-black rounded-l-sm border border-gray-300'
+        className='bg-white w-9 h-9 text-black rounded-l-sm border border-gray-300 disabled:opacity-50'
+        disabled={disabledLeft}
         onClick={onClickLeft}
       >
         &lt;
       </button>
       <button
-        className='bg-white w-9 h-9 text-black rounded-r-sm border border-gray-300'
+        className='bg-white w-9 h-9 text-black rounded-r-sm border border-gray-300 disabled:opacity-50'
+        disabled={disabledRight}
         onClick={onClickRight}
       >
         &gt;
