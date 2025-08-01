706e7cb458ff230f50823a238a999fef1fdd7260
김이서 <12305025k@gmail.com>
2025-08-01 17:43:56 +0900

    chore: 404페이지에 마이대시보드로 가기 버튼 추가

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,21 @@
+'use client';
 import Image from 'next/image';
 import NotFoundLogo from '../../public/images/NotFound.png';
+import { Button } from '@/components/button/Button';
 
 const NotFound = () => {
+  const handleButtonClick = () => {
+    window.location.href = '/mydashboard';
+  };
+
   return (
     <main className='w-screen h-screen grid place-items-center'>
       <div className='w-2xl text-center space-y-6'>
         <Image src={NotFoundLogo} alt='찾을 수 없습니다 이미지' />
         <span className='text-2xl-bold'>🤔 페이지를 찾을 수 없어요!</span>
+        <Button className='mt-2' size='large' variant='primary' onClick={handleButtonClick}>
+          돌아가기
+        </Button>
       </div>
     </main>
   );
